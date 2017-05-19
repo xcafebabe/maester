33ec833986d07bd442a7f9bc7a84f89c84b7180d
Luis Toubes <luis@toub.es>
2017-05-19 12:38:53 +0200

    added a new sample node-red-contribution

---
+++ src/sample2/lower-case.js
@@ -0,0 +1,14 @@
+
+export default (RED) => {
+  class LowerCaseNode {
+    constructor(config) {
+      RED.nodes.createNode(this, config)
+      var node = this
+      node.on('input', function (msg) {
+        msg.payload = msg.payload.toLowerCase()
+        node.send(msg)
+      })
+    }
+  }
+  RED.nodes.registerType("lower-case", LowerCaseNode);
+}
